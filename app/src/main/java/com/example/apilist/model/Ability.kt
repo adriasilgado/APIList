960d76b30693e6c33d15528546a9946421aa2ccb
sjo <adria.silgado.7e7@itb.cat>
2024-02-06 13:49:50 +0100

    api y implementaciones

---
+++ app/src/main/java/com/example/apilist/model/Ability.kt
@@ -0,0 +1,8 @@
+package com.example.apilist.model
+
+data class Ability(
+    val description: String,
+    val displayIcon: String,
+    val displayName: String,
+    val slot: String
+)
---
+++ app/src/main/java/com/example/apilist/model/ValorantAgentes.kt
@@ -0,0 +1,6 @@
+package com.example.apilist.model
+
+data class ValorantAgentes(
+    val `data`: List<Data>,
+    val status: Int
+)
