24e13ece18605ed6eb17481e406f885385eeee5f
sjo <adria.silgado.7e7@itb.cat>
2024-02-09 10:43:57 +0100

    fondo de imagen

--- app/src/main/java/com/example/apilist/view/SearchScreen.kt
+++ app/src/main/java/com/example/apilist/view/SearchScreen.kt
@@ -1,29 +1,43 @@
 package com.example.apilist.view
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import com.example.apilist.R
 import com.example.apilist.model.Data
 import com.example.apilist.model.ValorantAgentes
 import com.example.apilist.navigation.Routes
@@ -60,24 +74,47 @@ fun CharacterItem(character: Data) {
     if (character.isPlayableCharacter) {
         Card(
             border = BorderStroke(2.dp, Color.LightGray),
+            colors = CardDefaults.cardColors(Color.Transparent),
             shape = RoundedCornerShape(8.dp),
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .padding(8.dp)
         ) {
-            Row(modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()) {
-                GlideImage(
-                    model = character.displayIcon,
-                    contentDescription = "Character Image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(100.dp)
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.fondofin),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
                 )
-                Text(
-                    text = character.displayName,
-                    style = MaterialTheme.typography.bodyLarge,
-                    textAlign = TextAlign.Center, modifier = Modifier.fillMaxSize()
-                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    GlideImage(
+                        model = character.displayIcon,
+                        contentDescription = "Character Image",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.size(100.dp)
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    Text(
+                        text = character.displayName,
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                }
             }
         }
     }
 }
+
+
